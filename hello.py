9cae0d609b6f1bb12cb366234c617110c9c801b5
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-02-02 08:19:04 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ app.secret_key = ' key'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'momgeetha1'
+app.config['MYSQL_PASSWORD'] = '*******'
 app.config['MYSQL_DB'] = 'lms'
 
 # Intialize MySQL
