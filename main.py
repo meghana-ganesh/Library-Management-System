e3a8208c67fdb8675ee72e4ff0543fafa44b6650
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-02-02 08:39:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 
-mycon = mysql.connector.connect(host="localhost",user="root",passwd = "momgeetha1",database = "lms")
+mycon = mysql.connector.connect(host="localhost",user="root",passwd = "******",database = "lms")
 
 if mycon.is_connected():
     print('successfully connected to mysql database')
